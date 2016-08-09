ea61a0cfe18f0cc75ec97348d9336a9da95fbcbe
somethingscripted <bkinga@vols.utk.edu>
2016-08-08 20:01:34 -0500

    Fixed button range (for the most part)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tappers
+A whole new way to tap!
+
+Tap as many randomly-appearing button as you can. But hurry: you only got 60 seconds!
---
+++ app/src/main/java/com/example/benjamin/tappers/PlayActivity.java
@@ -0,0 +1,69 @@
+package com.example.benjamin.tappers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.view.View;
+
+import java.util.Random;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        final Button theButton = (Button) findViewById(R.id.theButton);
+        theButton.setText("");
+
+        final RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(320,320,0,0);
+
+        theButton.setLayoutParams(lp);
+        theButton.setText("");
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200,
+                r.getDisplayMetrics()
+                );
+
+
+
+        theButton.setOnClickListener(new Button.OnClickListener() {
+            int n = 0;
+            public void onClick(View v)
+            {
+                String num =  Integer.toString(n);
+                changeLocation(theButton, lp);
+                // theButton.setText(num);
+                // n++;
+
+            }
+        });
+
+
+    }
+
+    public void changeLocation(Button b, RelativeLayout.LayoutParams l) {
+
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Random rand = new Random();
+
+        // I took 25% of the phone's display and that seems to work (for now) Need to figure out how to acquire activity's display range in pixels.
+        int randWidth = rand.nextInt((int) Math.ceil(metrics.widthPixels*0.80));
+        int randHeight = rand.nextInt((int) Math.ceil(metrics.heightPixels*0.80));
+
+        l.setMargins(randWidth, randHeight, 0, 0);
+        b.setLayoutParams(l);
+    }
+
+
+}
